80e97c2567ff36682a958eb25aecaa5154f3e9e6
elisa <elisakarateka@gmail.com>
2023-05-10 23:18:10 +0200

    transi quand survol ligue card

--- src/components/LigueCard.vue
+++ src/components/LigueCard.vue
@@ -1,52 +1,60 @@
 <template>
-    <div class="ligue-card" v-if="image" :class="{ selected: index === currentIndex }" @click="handleClick">
-        <div>
-            <img class="image" :src="image" :alt="ligue">
-        </div>
-        <div class="ligue-elt">
-        <h1> {{ ligue }}</h1>
-        </div>
-      
+  <div
+    class="ligue-card"
+    v-if="image"
+    :class="{ selected: index === currentIndex }"
+    @click="handleClick"
+  >
+    <div>
+      <img class="image" :src="image" :alt="ligue" />
     </div>
-  </template>
-  
-  <script>
-  export default{
-    name : 'LigueCard',
-    props : {
-      ligue : {type : String, required: true},
-      image : {type: String, default: "https://gamingnewz.fr/wp-content/uploads/2020/04/Logo-lfl.png"},
-      index: { type: Number, required: true },
-      currentIndex: { type: Number, required: true },
-    },
+    <div class="ligue-elt">
+      <h1>{{ ligue }}</h1>
+    </div>
+  </div>
+</template>
 
-    methods: {
-      handleClick() {
-        this.$emit("update-current-index", this.index);
-      }
+<script>
+export default {
+  name: "LigueCard",
+  props: {
+    ligue: { type: String, required: true },
+    image: {
+      type: String,
+      default: "https://gamingnewz.fr/wp-content/uploads/2020/04/Logo-lfl.png",
     },
-    computed: {
+    index: { type: Number, required: true },
+    currentIndex: { type: Number, required: true },
+  },
+
+  methods: {
+    handleClick() {
+      this.$emit("update-current-index", this.index);
+    },
+  },
+  computed: {
     isSelected() {
-      return typeof this.currentIndex !== 'undefined' && this.index === this.currentIndex;
-    }
-  }
-  }
-  </script>
-  
-  <style scoped>
+      return (
+        typeof this.currentIndex !== "undefined" &&
+        this.index === this.currentIndex
+      );
+    },
+  },
+};
+</script>
 
-@media only screen and (min-width: 1024px){
-    .ligue-card {
+<style scoped>
+@media only screen and (min-width: 1024px) {
+  .ligue-card {
     position: relative;
     list-style: none;
     color: #ffffff;
     display: flex;
     cursor: pointer;
     background: #6b83a4;
-    box-shadow: inset 5px 5px 5px #9bbeee,
-              inset -5px -5px 5px #9bbeee;
+    box-shadow: inset 5px 5px 5px #9bbeee, inset -5px -5px 5px #9bbeee;
   }
-    .image {
+  .image {
     height: 5vw;
     width: 5vw;
     margin-left: 2vw;
@@ -56,28 +64,30 @@
   .ligue-elt h1 {
     font-size: 1.5vw;
     position: relative;
-    color: #282A35;
+    color: #282a35;
     margin-top: -1vw;
   }
 
   .selected {
     background: #607694;
-    box-shadow: inset 10px 10px 17px #3b485a,
-            inset -10px -10px 17px #3b485a
+    box-shadow: inset 10px 10px 17px #3b485a, inset -10px -10px 17px #3b485a;
   }
 }
 
-  @media only screen and (max-width: 767px){
-    .ligue-card {
+@media only screen and (max-width: 767px) {
+  .ligue-card {
     list-style: none;
     color: #ffffff;
     display: flex;
     cursor: pointer;
     background: #6b83a4;
-    box-shadow: inset 5px 5px 5px #9bbeee,
-              inset -5px -5px 5px #9bbeee;
+    transition: transform 0.2s ease-in-out;
+    box-shadow: inset 2px 2px 2px #9bbeee, inset -5px -5px 5px #9bbeee;
   }
-    .image {
+  .ligue-card:hover {
+    transform: scale(1.1);
+  }
+  .image {
     height: 7vw;
     width: 7vw;
     margin-left: 3vw;
@@ -85,28 +95,29 @@
 
   .ligue-elt h1 {
     font-size: 2.5vw;
-    color: #282A35;
+    color: #282a35;
     margin-top: -4vw;
   }
   .selected {
     background: #607694;
-    box-shadow: inset 10px 10px 17px #3b485a,
-            inset -10px -10px 17px #3b485a
+    box-shadow: inset 10px 10px 17px #3b485a, inset -10px -10px 17px #3b485a;
   }
+}
 
-  }
-
-  @media only screen and (max-width: 1023px) and (min-width: 768px){
-    .ligue-card {
+@media only screen and (max-width: 1023px) and (min-width: 768px) {
+  .ligue-card {
     list-style: none;
     color: #ffffff;
     display: flex;
     cursor: pointer;
     background: #6b83a4;
-  box-shadow: inset 5px 5px 5px #9bbeee,
-              inset -5px -5px 5px #9bbeee;
+    transition: transform 0.2s ease-in-out;
+    box-shadow: inset 2px 2px 2px #9bbeee, inset -5px -5px 5px #9bbeee;
   }
-    .image {
+  .ligue-card:hover {
+    transform: scale(1.05);
+  }
+  .image {
     height: 7vw;
     width: 7vw;
     margin-left: 3vw;
@@ -114,17 +125,12 @@
 
   .ligue-elt h1 {
     font-size: 2.5vw;
-    color: #282A35;
+    color: #282a35;
     margin-top: -2vw;
   }
   .selected {
     background: #6b83a4;
-  box-shadow: inset 10px 10px 17px #3b485a,
-            inset -10px -10px 17px #3b485a
+    box-shadow: inset 10px 10px 17px #3b485a, inset -10px -10px 17px #3b485a;
   }
-
-  }
-
-  </style>
-  
-  
+}
+</style>
